51d39369aabd7f7b4acd8b454c6f2e38ab6a9e3a
Kitsukei <124148621+Kitsukeii@users.noreply.github.com>
2023-12-29 16:23:27 +0000

    Update main.tf
    Updated credntials variables

--- main.tf
+++ main.tf
@@ -5,13 +5,13 @@ terraform {
    }
 }
 
-#provider "azurerm" {
-#   subscription_id = var.subscription_id
-#   client_id = var.client_id
-#   client_secret = var.client_secret
-#   tenant_id = var.tenant_id
-#   features {}
-#}
+provider "azurerm" {
+   subscription_id = $AZURE_SUBSCRIPTION_ID
+   client_id = $AZURE_CLIENT_ID
+   client_secret = $AZURE_CLIENT_SECRET
+   tenant_id = $AZURE_TENANT_ID
+   features {}
+}
 
 resource "azurerm_resource_group" "webserver" {
    name = "nginx-server"
