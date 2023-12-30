46015b1e91414d7af824a2766480a144a67aca56
Kitsukei <124148621+Kitsukeii@users.noreply.github.com>
2023-12-30 10:47:21 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ terraform {
 }
 
 provider "azurerm" {
-   subscription_id = $AZURE_SUBSCRIPTION_ID
-   client_id = $AZURE_CLIENT_ID
-   client_secret = $AZURE_CLIENT_SECRET
-   tenant_id = $AZURE_TENANT_ID
+   subscription_id = var.subscription_id
+   client_id = var.client_id
+   client_secret = var.client_secret
+   tenant_id = var.tenant_id
    features {}
 }
 
