61a57644c48d539c6da5cafff91cfabce5b5bf0e
Kitsukei <124148621+Kitsukeii@users.noreply.github.com>
2024-01-07 13:18:48 +0000

    Update Swarm.tf

--- Swarm.tf
+++ Swarm.tf
@@ -1,67 +1,5 @@
-terraform {
-   required_version = ">= 0.12"
-   required_providers {
-      azurerm = "~>2.24.0"
-   }
-}
-
-provider "azurerm" {
-   subscription_id = var.subscription_id
-   client_id = var.client_id
-   client_secret = var.client_secret
-   tenant_id = var.tenant_id
-   features {}
-}
-
-resource "azurerm_resource_group" "webserver" {
-   name = "nginx-server"
-   location = "West US"
-}
-
-resource "azurerm_network_security_group" "allowedports" {
-   name = "allowedports"
-   resource_group_name = azurerm_resource_group.webserver.name
-   location = azurerm_resource_group.webserver.location
-  
-   security_rule {
-       name = "http"
-       priority = 100
-       direction = "Inbound"
-       access = "Allow"
-       protocol = "Tcp"
-       source_port_range = "*"
-       destination_port_range = "80"
-       source_address_prefix = "*"
-       destination_address_prefix = "*"
-   }
-
-   security_rule {
-       name = "https"
-       priority = 200
-       direction = "Inbound"
-       access = "Allow"
-       protocol = "Tcp"
-       source_port_range = "*"
-       destination_port_range = "443"
-       source_address_prefix = "*"
-       destination_address_prefix = "*"
-   }
-
-   security_rule {
-       name = "ssh"
-       priority = 300
-       direction = "Inbound"
-       access = "Allow"
-       protocol = "Tcp"
-       source_port_range = "*"
-       destination_port_range = "22"
-       source_address_prefix = "*"
-       destination_address_prefix = "*"
-   }
-}
-
-resource "azurerm_public_ip" "webserver_public_ip" {
-   name = "webserver_public_ip"
+resource "azurerm_public_ip" "webserver_public_ip2" {
+   name = "webserver_public_ip2"
    location = "West US"
    resource_group_name = azurerm_resource_group.webserver.name
    allocation_method = "Dynamic"
@@ -74,8 +12,8 @@ resource "azurerm_public_ip" "webserver_public_ip" {
    depends_on = [azurerm_resource_group.webserver]
 }
 
-resource "azurerm_network_interface" "webserver" {
-   name = "nginx-interface"
+resource "azurerm_network_interface" "webserver2" {
+   name = "nginx-interface2"
    location = azurerm_resource_group.webserver.location
    resource_group_name = azurerm_resource_group.webserver.name
 
@@ -83,20 +21,20 @@ resource "azurerm_network_interface" "webserver" {
        name = "internal"
        private_ip_address_allocation = "Dynamic"
        subnet_id = module.network.vnet_subnets[0]
-       public_ip_address_id = azurerm_public_ip.webserver_public_ip.id
+       public_ip_address_id = azurerm_public_ip.webserver_public_ip2.id
    }
 
    depends_on = [azurerm_resource_group.webserver]
 }
 
-resource "azurerm_linux_virtual_machine" "nginx" {
+resource "azurerm_linux_virtual_machine" "nginx2" {
    size = "Standard_F2"
-   name = "nginx-webserver"
+   name = "nginx-webserver2"
    resource_group_name = azurerm_resource_group.webserver.name
    location = azurerm_resource_group.webserver.location
    custom_data = base64encode(file("init.sh"))
    network_interface_ids = [
-       azurerm_network_interface.webserver.id,
+       azurerm_network_interface.webserver2.id,
    ]
 
    source_image_reference {
@@ -112,7 +50,7 @@ resource "azurerm_linux_virtual_machine" "nginx" {
    disable_password_authentication = false
 
    os_disk {
-       name = "nginxdisk01"
+       name = "nginxdisk201"
        caching = "ReadWrite"
        storage_account_type = "Standard_LRS"
    }
